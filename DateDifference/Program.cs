24448caf8db903323dc7035749a0c36488a78c76
Melissa DoddRuesing <melissa.doddruesing@gmail.com>
2018-08-17 17:16:28 -0500

    CG 6-4

--- DateDifference/Program.cs
+++ DateDifference/Program.cs
@@ -6,7 +6,31 @@ namespace DateDifference
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //create a program that counts the number of days between two different dates both inpyt by the user
+            Console.WriteLine("Please enter first date in format of: MM/DD/YYYY");
+            string dateString = Console.ReadLine();
+
+            //DateTime dateString = DateTime.Parse(Console.Readline());
+            //or
+
+            DateTime date1 = DateTime.Parse(dateString);
+            Console.WriteLine("Enter second date");
+            string dateString2 = Console.ReadLine();
+            DateTime date2 = DateTime.Parse(dateString2);
+            //TimeSpan difference = date1 - date2        
+            //or
+            TimeSpan difference = date1.Subtract(date2);
+            Console.WriteLine("The two dates are " + Math.Abs(difference.Days) + " days apart.");
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
         }
     }
 }
